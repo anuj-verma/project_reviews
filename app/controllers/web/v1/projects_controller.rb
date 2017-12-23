920332ffab6c6e839759298de7766b2b1b3c3edf
Anuj Verma <anujver23@gmail.com>
2017-12-23 12:54:36 +0530

    Adds API namespace for Web::V1 Apis and Project Controller

---
+++ app/controllers/web/v1/projects_controller.rb
@@ -0,0 +1,4 @@
+class Web::V1::ProjectsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  api_version(
+    module: 'Web::V1',
+    header: {
+      name: 'Accept',
+      value: 'application/vnd.project_reviews.com; version=web_v1'
+    }
+  ) do
+    resources :projects, only: [:index, :create, :edit, :update] do
+      resources :reviews, only: [:index, :create, :edit, :update]
+    end
+  end
 end
