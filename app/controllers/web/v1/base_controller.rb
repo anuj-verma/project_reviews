ce3ccb7f0d52fb9de5deac05677283062fd963e9
Anuj Verma <anujver23@gmail.com>
2017-12-23 13:18:29 +0530

    Adds common response and serializer methods in Web::V1::BaseController

--- app/controllers/web/v1/base_controller.rb
+++ app/controllers/web/v1/base_controller.rb
@@ -1,6 +1,17 @@
 module Web
   module V1
     class BaseController < ApplicationController
+      def serialize_resource(resource, serializer)
+        options = { each_serializer: serializer }
+        ActiveModelSerializers::SerializableResource.new(resource, options).as_json
+      end
+
+      def success_response(data:, status_code:)
+        render json: {
+          data: data,
+        },
+        status: status_code
+      end
     end
   end
 end
--- app/controllers/web/v1/projects_controller.rb
+++ app/controllers/web/v1/projects_controller.rb
@@ -1,7 +1,14 @@
 module Web
   module V1
     class ProjectsController < Web::V1::BaseController
+      PROJECT_SERIALIZER = Web::V1::ProjectSerializer
+
       def index
+        projects = Project.all
+        success_response(
+          data: serialize_resource(projects, PROJECT_SERIALIZER),
+          status_code: :ok
+        )
       end
     end
   end
